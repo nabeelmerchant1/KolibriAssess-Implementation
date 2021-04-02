06e1afd237435875289ba4e705aae7eb2e8fbc27
Nabeel Tad Merchant <40885903+nabeelmerchant1@users.noreply.github.com>
2021-04-02 18:48:31 +0500

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Vuetify from 'Vuetify';
+import VueEvents from 'vue-events'
+
+import App from './App';
+import router from './router';
+import store from './store';
+import './auth';
+
+import('vuetify/dist/vuetify.css');
+
+Vue.use(Vuetify);
+Vue.use(VueEvents);
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  template: '<App/>',
+  components: { App }
+});
